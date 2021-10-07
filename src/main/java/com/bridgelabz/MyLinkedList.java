d2ba3814fe127207b843e7505a0e61fd1f06114f
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-10-08 00:49:40 +0530

    ADDED:Created A simple LinkedList

---
+++ src/main/java/com/bridgelabz/MyLinkedList.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class MyLinkedList {
+    public static void main(String[] args) {
+        LinkedListed list = new LinkedListed();
+        list.addNode(56);
+        list.addNode(30);
+        list.addNode(70);
+        list.display();
+    }
+
+}
